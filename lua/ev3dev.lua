3445711be0a97b5839effa42cb4e18e5887de2e7
Franz Detro <franz.detro@gmx.de>
2014-05-15 21:39:34 +0200

    adjusted lua implementation to the current state of cpp

--- lua/ev3dev.lua
+++ lua/ev3dev.lua
@@ -43,6 +43,7 @@ function Device:getAttrInt(name)
 		local tf = io.open(self._path..name, "r")
 		if (tf ~= nil) then
 			result = tf:read("*n")
+			tf:close()
 		end
   end
   
@@ -88,23 +89,13 @@ end
 ------------------------------------------------------------------------------
 -- Sensor
 
-Sensor = class()
+Sensor = class(Device)
 
-function Sensor:init(sensor_value_fname, port)
+function Sensor:init(sensor_path)
 
-	self._fname = sensor_value_fname
+	self._path = sensor_path
 	self._type = 0
 	self._port = 0
-	
-	local f = io.open(self._fname)
-	if (f ~= nil) then
-	  if (port ~= 0) then
-		  self._port = port
-		else
-		  self._port = 1
-		end
-		f:close()
-	end
 	
 end
 
@@ -144,20 +135,19 @@ function MSensor:init(sensor_type, port)
 	self._port = 0
 
 	for i = 0, 9 do
-		self._dname = sys_msensor.."sensor"..i.."/"
+		self._path = sys_msensor.."sensor"..i.."/"
 		
-		local tf = io.open(self._dname.."type_id", "r")
+		local tf = io.open(self._path.."type_id", "r")
 		if (tf ~= nil) then
 			self._type = tf:read("*n")
 			
 			if ((sensor_type == nil) or (sensor_type == 0) or (self._type == sensor_type)) then
-				local pf = io.open(self._dname.."port_name", "r")
+				local pf = io.open(self._path.."port_name", "r")
 				self._port = string.match(pf:read(), "%d+")
 				pf:close()
 
 				if ((port == nil) or (port == 0) or (self._port == port)) then
-				  self._fname = sys_msensor.."sensor"..i.."/value"
-				  break;
+				  break
 				else
 					self._port = 0
 				end
@@ -167,54 +157,24 @@ function MSensor:init(sensor_type, port)
 end
 
 function MSensor:mode()
-	if (self._port ~= 0) then
-		local mf = io.open(self._dname.."mode", "r")
-		local m = string.match(mf:read(), "%[%w+[%-%w+]*%]")
-		if (m.len) then
-			return string.match(m, "%w+[%-%w+]*")
-		end
-	end
-	return ""
+  return self:getAttrString("mode")
 end
 
 function MSensor:modes()
-	if (self._port ~= 0) then
-		local mf = io.open(self._dname.."mode", "r")
-		return mf:read()
-	end
-	return ""
+  return self:getAttrString("modes")
 end
 
 function MSensor:setMode(mode)
-	if (self._port ~= 0) then
-		local mf = io.open(self._dname.."mode", "w")
-		mf:write(mode)
-		mf:close()
-	end
+  self:setAttrString("mode", mode)
 end
 
 function MSensor:value(id)
-	if (self._port ~= 0) then	
-		if (id == nil) then
-			id = 0
-		end
 
-		if (id == 0) then
-			local fvalue = io.open(self._fname..id, "r")
-			local val = fvalue:read("*n")
-			fvalue:close()
-			return val
-		else		
-			local vf = io.open(self._dname.."value"..id, "r")
-			if (vf ~= nil) then
-				local val = vf:read("*n")
-				vf:close()
-				return val
-			end
-		end
+	if (id == nil) then
+		id = 0
 	end
-	
-	return 0
+
+  return self:getAttrInt("value"..id)
 end
 
 ------------------------------------------------------------------------------
@@ -380,6 +340,10 @@ function Motor:position()
   return self:getAttrInt("position")
 end
 
+function Motor:setPosition(value)
+  self:setAttrInt("position", value)
+end
+
 function Motor:pulsesPerSecond()
   return self:getAttrInt("pulses_per_second")
 end
@@ -448,7 +412,7 @@ function Motor:setTimeSetpoint(value)
   self:setAttrInt("time_setpoint", value)
 end
 
-function Motor:position_setpoint()
+function Motor:positionSetpoint()
   return self:getAttrInt("position_setpoint")
 end
 
@@ -493,37 +457,22 @@ end
 ------------------------------------------------------------------------------
 -- LED
 
-LED = class()
+LED = class(Device)
 
 function LED:init(name)
-  self._dname = sys_class.."leds/ev3:"..name.."/"
+  self._path = sys_class.."leds/ev3:"..name.."/"
 end
 
 function LED:level()
-	local file = io.open(self._dname.."brightness", "r")
-	if (file ~= nil) then	
-		local val = file:read("*n")
-		file:close()
-		return val
-	end
-	
-	return 0
+  return self:getAttrInt("brightness")
 end
 
 function LED:on()
-	local file = io.open(self._dname.."brightness", "w")
-	if (file ~= nil) then	
-		file:write("1")
-		file:close()
-	end
+  self:setAttrInt("brightness", 1)
 end
 
 function LED:off()
-	local file = io.open(self._dname.."brightness", "w")
-	if (file ~= nil) then	
-		file:write("0")
-		file:close()
-	end
+  self:setAttrInt("brightness", 0)
 end
 
 function LED:flash(interval)
@@ -535,23 +484,15 @@ function LED:flash(interval)
 end
 
 function LED:setOnDelay(ms)
-	local file = io.open(self._dname.."delay_on", "w")
-	if (file ~= nil) then	
-		file:write(tostring(ms))
-		file:close()
-	end
+  self:setAttrInt("delay_on", ms)
 end
 
 function LED:setOffDelay(ms)
-	local file = io.open(self._dname.."delay_off", "w")
-	if (file ~= nil) then	
-		file:write(tostring(ms))
-		file:close()
-	end
+  self:setAttrInt("delay_off", ms)
 end
   
 function LED:trigger()
-	local file = io.open(self._dname.."trigger", "r")
+	local file = io.open(self._path.."trigger", "r")
 	if (file ~= nil) then	
 		local m = string.match(file:read(), "%[%w+[%-%w+]*%]")
 		file:close()
@@ -564,13 +505,12 @@ function LED:trigger()
 end
 
 function LED:triggers()
-	local file = io.open(self._dname.."trigger", "r")
+	local file = io.open(self._path.."trigger", "r")
 	if (file ~= nil) then	
 		local m = file:read()
 		file:close()
 		if (m.len) then
 		  return m
-		--	return string.match(m, "%w+[%-%w+]*")
 		end
 	end
 	
@@ -578,11 +518,7 @@ function LED:triggers()
 end
 
 function LED:setTrigger(trigger)
-	local file = io.open(self._dname.."trigger", "w")
-	if (file ~= nil) then	
-		file:write(trigger)
-		file:close()
-	end
+  self:setAttrString("trigger", trigger)
 end
 
 ledRedRight   = LED("red:right")
