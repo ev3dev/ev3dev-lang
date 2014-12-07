b34c5272b8b99c6c75f5d476d3e1321b8d98adaf
Bartosz Meglicki <bmegli@op.pl>
2014-12-07 15:05:45 +0100

    examples for SER

--- R/examples/SER.R
+++ R/examples/SER.R
@@ -6,6 +6,14 @@ rtouch=touch.sensor(ports$INPUT_3)
 infrared=infrared.sensor(ports$INPUT_AUTO)
 gyro=xg1300l.sensor(ports$INPUT_AUTO)
 
+SpeakPL=function(..., sync=TRUE)
+{
+  text=paste(list(...), collapse="")
+  command=paste("espeak -a 200 -v pl --stdout \"", text, "\" | aplay", collapse="")  
+  if(!sync) command=paste(command, "&")
+  system(command, intern=TRUE, ignore.stderr=TRUE)
+}
+
 InitDriveMotor=function(m)
 {
   SetRegulationMode(m, "on")
@@ -62,7 +70,7 @@ FollowInfrared=function()
   StopEngines(left, right)
 }
 
-RemoteControl=function()
+RemoteControl=function(inf_thr=40)
 {
   SetMode(infrared, "IR-REMOTE")
   SetRunMode(left, "forever")  
@@ -80,15 +88,203 @@ RemoteControl=function()
   {
     sleft=switch(button+1, 0L,  300L, -300L, 0L, 0L, 300L, 300L, -300L, -300L, 0L, 0L, 0L)
     sright=switch(button+1, 0L,  0L, 0L, 300L, -300L, 300L, -300L, 300L, -300L, 0L, 0L, 0L)
-    
+ 
+    if((Value(ltouch)==0L || Value(rtouch)==0L) && (sleft>0 || sright>0) )
+      sleft=sright=0L
+        
     SetPulsesPerSecondSP(left, sleft)
     SetPulsesPerSecondSP(right, sright)    
-    
-    Sys.sleep(0.1)
+      
   }
   StopEngines(left, right)
 }
 
+CMToDrivePosition=function(cm) {as.integer(cm*100*5/12)}
+DegreeToDrivePosition=function(degree){ as.integer(-degree * 520 / 90) }
+
+DriveStart=function(left_motor, right_motor, cm)
+{    
+  pos=CMToDrivePosition(cm)
+  SetPositionSP(left_motor, pos)
+  SetPositionSP(right_motor, pos)
+  
+  Run(left_motor)
+  Run(right_motor)       
+}
+
+
+TestDutyCycle=function(cm)
+{  
+  size=100L
+  readings=1L
+  
+  left_states=character(size)
+  right_states=character(size)
+  ldc=integer(size)
+  rdc=integer(size)
+  
+  SetModeToPosition(left)
+  SetModeToPosition(right)
+  
+  DriveStart(left, right, cm)  
+  
+  while(Running(left) || Running(right))
+  {
+    left_states[readings]=State(left)
+    right_states[readings]=State(right)
+    ldc[readings]=DutyCycle(left)
+    rdc[readings]=DutyCycle(right)
+    readings=readings+1L
+    
+    #Sys.sleep(0.01)
+  }    
+  
+  span=1L:(readings-1L)
+  
+  data.frame(left_states = left_states[span], right_states = right_states[span], ldc=ldc[span], rdc=rdc[span] )
+}
+
+DriveDutyCycle=function(cm, ldc.thr, rdc.thr)
+{
+  pos=CMToDrivePosition(cm)
+  control=list(lgo=pos, rgo=pos, pps.sp=300L, ldc.thr=ldc.thr, rdc.thr=rdc.thr)
+  
+  DriveSafely2(control)
+}
+
+MotorFactor=function(m)
+{
+  factor(State(m), c("idle", "ramp_const", "ramp_down", "ramp_up", "position_ramp_down"))
+}
+
+
+GetReadings2=function(left, right, gyro, infrared, ltouch, rtouch)
+{
+  c(Value(infrared), Value(gyro), Value(ltouch), Value(rtouch), DutyCycle(left), DutyCycle(right), MotorFactor(left), MotorFactor(right), Position(left), Position(right) )
+}
+
+
+CheckConditions2=function(readings, control)
+{  
+  r=readings
+  c=control
+    
+#c(Value(infrared), Value(gyro), Value(ltouch), Value(rtouch), DutyCycle(left), DutyCycle(right), MotorFactor(left), MotorFactor(right), Position(left), Position(right) )
+  list(cliff=r[1] > c$inf.up, obstacle=r[1] < c$inf.down,
+       ltouch = (r[3] == c$lt), rtouch = (r[4] == c$rt),  
+       ldc = r[7] == 2 && abs(r[5]) > c$ldc.thr,
+       rdc = r[8] == 2 && abs(r[6]) > c$rdc.thr)
+}
+
+
+PrepareEngines=function(left, right, control)
+{
+  if(!is.null(control$pps.sp))
+  {
+    SetPulsesPerSecondSP(left, control$pps.sp)
+    SetPulsesPerSecondSP(right, control$pps.sp)
+  }  
+  
+  SetPosition(left, 0)
+  SetPosition(right, 0)
+  SetStopMode(left, "break")  
+  SetStopMode(right, "break")
+}
+
+
+StopEngines=function(left, right)
+{
+  Stop(left)
+  Stop(right)
+}
+
+
+DriveStartControl=function(left, right, control)
+{    
+  if(is.null(control$lgo) | is.null(control$rgo))
+  {
+    SetRunMode(left, "forever")
+    SetRunMode(right, "forever")
+  }
+  else
+  {
+    SetRunMode(left, "position")
+    SetRunMode(right, "position")
+    SetPositionMode(left, "relative")
+    SetPositionMode(right, "relative")        
+    SetPositionSP(left, control$lgo)
+    SetPositionSP(right, control$rgo)    
+    SetPosition(left, 0)
+    SetPosition(right, 0)        
+  }  
+  
+  Run(left)
+  Run(right)       
+}
+
+
+SpeakWhy=function(check)
+{     
+  if(!is.na(check$ltouch) && check$ltouch==1 || !is.na(check$rtouch) && check$rtouch==1)
+    SpeakPL("Brak kontaktu")    
+  if(!is.na(check$cliff) && check$cliff)
+    SpeakPL("Przepaść")  
+  if(!is.na(check$obstacle) && check$obstacle)
+    SpeakPL("Przeszkoda")
+  if(!is.na(check$ldc) && check$ldc==1 || !is.na(check$rdc) && check$rdc==1)
+    SpeakPL("Kolizja")
+}
+
+
+PrepareControl=function(control)
+{
+  if(is.null(control$inf.up)) control$inf.up=NA
+  if(is.null(control$inf.down)) control$inf.down=NA
+  if(is.null(control$lt)) control$lt=NA
+  if(is.null(control$rt)) control$rt=NA
+  if(is.null(control$inf.var.thr)) control$inf.var.thr=NA
+  if(is.null(control$ldc.thr)) control$ldc.thr=100
+  if(is.null(control$rdc.thr)) control$rdc.thr=100  
+  if(is.null(control$pps.sp)) control$pps.sp=300      
+  if(is.null(control$get.readings)) control$get.readings=FALSE
+  control
+}
+
+
+DriveSafely2=function(control)
+{  
+  control=PrepareControl(control)
+  PrepareEngines(left, right, control)
+  gdata[1,]=GetReadings2(left, right, gyro, infrared, ltouch, rtouch)
+  if(any( unlist(CheckConditions2(gdata[1,], control)), na.rm=TRUE))
+    return (gdata[c(1,1),])
+  
+  r=2
+  DriveStartControl(left, right, control)
+  
+  while(Running(right) || (Running(left) ))
+  {
+    gdata[r,]=GetReadings2(left, right, gyro, infrared, ltouch, rtouch)
+    if(any( unlist(CheckConditions2(gdata[r,], control)), na.rm=TRUE))
+      StopEngines(left, right)
+    r=r+1
+  }
+  
+  SpeakWhy(CheckConditions2(gdata[r-1,], control))
+  
+  gdata[r,]=GetReadings2(left, right, gyro, infrared, ltouch, rtouch)
+  
+  
+  #  CheckInfraredVariance(infrared, control)
+  if(control$get.readings)
+    return (gdata[1:r,])
+  else
+    return (gdata[c(1,r),])
+}
+
+gdata=matrix(0L, nrow=1000L, ncol=10, dimnames=list(c(), c("inf", "head", "lt", "rt", "ldc", "rdc", "lst", "rst", "lpos", "rpos")))
+
 InitDriveMotor(left)
 InitDriveMotor(right)
+
 
--- R/examples/SER_client.R
+++ R/examples/SER_client.R
@@ -14,9 +14,41 @@ RS.eval(con, source("SER.R"))
 
 # Now we are ready for work
 
-RS.eval(con, FollowInfrared())
+## Remote Control
 
 RS.eval(con, RemoteControl())
 
+## Follow Infrared
+
+RS.eval(con, FollowInfrared())
+
+## Duty Cycle
+
+ndc=RS.eval(con, TestDutyCycle(20))
+hdc=RS.eval(con, TestDutyCycle(20))
+
+levels=unique(c(levels(ndc$left_states), levels(hdc$left_states), levels(ndc$right_states), levels(hdc$right_states)   ))
+
+par(mfcol=c(1, 2))
+
+plot(ndc$ldc, col=factor(ndc$left_states, levels), ylim=c(0,100),pch=20, cex=2, main="typical DC")
+plot(hdc$ldc, col=factor(hdc$left_states, levels), ylim=c(0,100),pch=20,cex=2, main="collision DC")
+
+par(mfcol=c(1, 1))
+
+min.x=-10+min(ndc$ldc[ndc$left_states=="ramp_const"], hdc$ldc[hdc$left_states=="ramp_const"])
+max.x=10+max(ndc$ldc[ndc$left_states=="ramp_const"], hdc$ldc[hdc$left_states=="ramp_const"])
+
+plot(density(ndc$ldc[ndc$left_states=="ramp_const"], bw = "SJ"), xlim=c(min.x, max.x), col="green", lwd=10, main="Duty Cycle - typical vs collision")
+lines(density(hdc$ldc[hdc$left_states=="ramp_const"], bw = "SJ"), lwd=10, col="red")
+
+ldc_threshold=min(hdc$ldc[hdc$left_states=="ramp_const"])
+rdc_threshold=min(hdc$rdc[hdc$right_states=="ramp_const"])
+
+for(i in 1:5)
+{
+  RS.eval(con, as.call(list(quote(DriveDutyCycle), 20, ldc_threshold, rdc_threshold)), lazy=FALSE)
+  RS.eval(con, as.call(list(quote(DriveDutyCycle), -20, ldc_threshold, rdc_threshold)), lazy=FALSE)
+}
 
 RS.close(con)
