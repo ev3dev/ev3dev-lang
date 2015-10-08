3275f493c69ad9598ad57b8b8c92c42fe2e8c14b
Ralph Hempel <rhempel@hempeldesigngroup.com>
2015-10-07 22:56:32 -0400

    Merged cpp changes from develop branch of ev3dev-lang

--- cpp/ev3dev-lang-test.cpp
+++ cpp/ev3dev-lang-test.cpp
@@ -22,6 +22,7 @@
 //-----------------------------------------------------------------------------
 //~autogen autogen-header
     // Sections of the following code were auto-generated based on spec v0.9.3-pre, rev 2. 
+
 //~autogen
 //-----------------------------------------------------------------------------
 
